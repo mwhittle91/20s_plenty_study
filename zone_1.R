8d7efb956ba45892c7d4d7eb8eece26419e0399e
Matthew Whittle <gy14mw@leeds.ac.uk>
2016-05-13 16:42:34 +0100

    added zone1 spatial study

--- zone_1.R
+++ zone_1.R
@@ -6,7 +6,13 @@ z_1_plus_3 <- seq(z_1_date, length = 2, by = "+3 months")[2]
 
 # find the required incidents
 z_1_pre_3months <- subset(zone_1_incidents_pre,  z_1_minus_3 < zone_1_incidents_pre@data$Date)
-z_1_post_3months <- subset(zone_1_incidents_pre,  z_1_plus_3 > zone_1_incidents_post@data$Date)
+z_1_post_3months <- subset(zone_1_incidents_post,  z_1_plus_3 > zone_1_incidents_post@data$Date)
+
+before_drop <- z_1_pre_3months[,-(10)]
+after_drop <- z_1_post_3months[-(10)]
+geojson_write(before_drop, geometry = "point", file = "before.geojson")
+geojson_write(after_drop, geometry = "point", file = "after.geojson")
+
 
 # how many incidents in each subset
 z1_3months_before <- as.numeric(nrow(z_1_pre_3months))
